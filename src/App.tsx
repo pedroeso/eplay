aa33cece8606fbbe109d8f89bb6ea9fa480b450e
Pedro Ribeiro <pedrorocha@gmail.com>
2025-03-26 18:15:31 -0300

    Criando a Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-import Banner from './components/Banner'
 import Header from './components/Header'
-import { GlobalCss, Container } from './styles'
-import Product from './components/Product'
-import ProductList from './components/ProductList'
+import { GlobalCss } from './styles'
+import Home from './pages/Home'
 
 const rotas = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <>
-        <Banner />
-        <ProductList title="Promoções" background="gray" />
-        <ProductList title="Em breve" background="black" />
-      </>
-    )
+    element: <Home />
   }
 ])
 
