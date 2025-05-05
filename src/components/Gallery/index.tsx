62a8dd6352f251787908e694b0878c33ab284ece
Pedro Ribeiro <pedrorocha@gmail.com>
2025-05-05 17:11:54 -0300

    pondo o icone de zoom

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,26 +1,59 @@
 import Section from '../Section'
 
-import { Item, Items } from './styles'
+import { Item, Items, Action } from './styles'
 
-import zelda from '../../assets/images/banner-homem-aranha.png'
+import spiderman from '../../assets/images/banner-homem-aranha.png'
+import hogwarts from '../../assets/images/fundo_hogwarts.png'
+import play from '../../assets/images/botao-play.png'
+import zoom from '../../assets/images/mais-zoom.png'
 
-const Gallery = () => (
-  <Section title="Galeria" background="black">
-    <Items>
-      <Item>
-        <img src={zelda} alt="imagem do link" />
-      </Item>
-      <Item>
-        <img src={zelda} alt="imagem do link" />
-      </Item>
-      <Item>
-        <img src={zelda} alt="imagem do link" />
-      </Item>
-      <Item>
-        <img src={zelda} alt="imagem do link" />
-      </Item>
-    </Items>
-  </Section>
-)
+type GalleryItem = {
+  type: 'video' | 'image'
+  url: string
+}
+
+const mock: GalleryItem[] = [
+  {
+    type: 'image',
+    url: spiderman
+  },
+  {
+    type: 'image',
+    url: hogwarts
+  },
+  {
+    type: 'video',
+    url: 'https://www.youtube.com/embed/uHGShqcAHlQ?si=P_NGYYcIoHsf2sM9'
+  }
+]
+
+type Props = {
+  defaultCover: string
+}
+
+const Gallery = ({ defaultCover }: Props) => {
+  const getMediaCover = (item: GalleryItem) => {
+    if (item.type === 'image') return item.url
+    return defaultCover
+  }
+
+  return (
+    <Section title="Galeria" background="black">
+      <Items>
+        {mock.map((media, index) => (
+          <Item key={media.url}>
+            <img
+              src={getMediaCover(media)}
+              alt={`Midia ${index + 1} de Nome do Jogo`}
+            />
+            <Action>
+              <img src={zoom} alt="Clique para maximar a imagem" />
+            </Action>
+          </Item>
+        ))}
+      </Items>
+    </Section>
+  )
+}
 
 export default Gallery
--- src/components/Gallery/styles.ts
+++ src/components/Gallery/styles.ts
@@ -5,14 +5,36 @@ export const Items = styled.ul`
   display: flex;
 `
 
+export const Action = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.73);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0;
+  transition: opacity 0.5s ease;
+`
+
 export const Item = styled.li`
   margin-right: 16px;
+  position: relative;
 
-  img {
+  > img {
     border: 2px solid ${cores.branca};
     border-radius: 8px;
     width: 150px;
     height: 150px;
     object-fit: cover;
+  }
+
+  &:hover {
+    ${Action} {
+      opacity: 1;
+      transition: opacity 0.5s ease;
+    }
   }
 `
